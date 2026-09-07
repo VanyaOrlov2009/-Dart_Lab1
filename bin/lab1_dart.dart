75900c2b01083fe903bd2379d697ba9743a63b1e
unknown <orlovvana776@gmail.com>
2026-09-07 13:22:52 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,58 +1,50 @@
 void main() {
-  String name = "Ванек";
-  int age = 17;
-  double height = 1.87;
+  String name = "Артем";
+  int age = 20;
+  double height = 1.75;
   bool isStudent = true;
-
+  
   print(name);
   print(age);
   print(height);
   print(isStudent);
-
-  print('Привет, $name! Тебе $age лет.');
+  
+  print('Привет, $name! Teбe $age лет.');
   print('Через 5 лет тебе будет ${age + 5} лет.');
-  print('Рост: ${height} м, студент: ${isStudent}');
-
+  print('Рост: $height м, студент: $isStudent');
+  var count = 0;
+  var title = 'dart';
   var score = 95;
-  // var language = 'Dart';
-
-  final String language = 'Dart';
-  final int releaseYear = 2011;
+  var language = 'Dart';
   print('$language: $score');
-
-  const String appName = "Lab1";
+  const String appName = 'Lab1';
   final int startYear = 2026;
-  print("$appName started in $startYear");
-
-  String? city = null; 
-
+  print('$appName started in $startYear');
+  String? city = null;
   if (city != null) {
-    print(city.toUpperCase(),
-    );  
+    print(
+      city.toUpperCase(),
+    );
   }
- 
-  print(city?.toUpperCase());
+String? nickname = null;
+String display = 
+    nickname ?? 'Аноним';
+print(display);
+List<String> fruits = ['яблоко','банан','груша'];
+fruits.add('апельсин');
+print(fruits[0]);
+print(fruits.length);
+Map<String,dynamic> person = {'name':'Артем','age':20};
+print(person['name']);
+person['city'] = 'Волжский';
+Set<int> ids = {1,2,3,2,1};
+print(ids);
+print(ids.length);
+List<String> fruits2 = ['яблоко','банан','груша'];
+for (var fruit in fruits2) {
+  print(fruit);
+}
 
-  String? nickname = null;
-  String display = nickname ?? 'Аноним';
-  print(display);
 
-  List<String> fruits = ["яблоко", "банан", "груша",];
-  fruits.add("апельсин");
-  print(fruits[0]);
-  print(fruits.length);
-
-  Map<String, dynamic> person = {'name': 'Артём', 'age': 20};
-  print(person['name']);
-  person['city'] = 'Волжский';
-
-  Set<int> ids = {1, 2, 3, 2, 1};
-  print(ids);
-  print(ids.length);
-
-  List<String> fruits2 = ['яблоко', 'банан', 'груша',];
-  for(var fruit in fruits2) {
-    print(fruit);
-  }
 
 }
